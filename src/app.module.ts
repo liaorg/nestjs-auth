09ee00b434a3efdac5e45296af20096cbf7ddb33
liaorg <liaorg@heidun.net>
2022-08-03 18:19:36 +0800

    1. 增加 i18n 方案 2. 增加静态服务 3. 规范目录结构：移动users到modules目录

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,35 @@
 import { Module } from "@nestjs/common";
 import { APP_FILTER, APP_INTERCEPTOR } from "@nestjs/core";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { I18nModule } from "nestjs-i18n";
+import path from "path";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AnyExceptionFilter, HttpExceptionFilter } from "./common/filters";
 import { TransformInterceptor } from "./common/interceptors";
-import { UsersModule } from "./users/users.module";
+import { UsersModule } from "./modules/users/users.module";
 
 @Module({
-    imports: [UsersModule],
+    imports: [
+        // 静态服务
+        // npm install --save @nestjs/serve-static
+        // https://docs.nestjs.com/recipes/serve-static
+        ServeStaticModule.forRoot({
+            rootPath: `${__dirname}/../public`,
+            renderPath: "/",
+        }),
+        // 国际化 i18n
+        // npm install --save nestjs-i18n
+        // https://github.com/toonvanstrijp/nestjs-i18n
+        I18nModule.forRoot({
+            fallbackLanguage: "zh-CN",
+            loaderOptions: {
+                path: path.join(__dirname, "/i18n/"),
+                watch: true,
+            },
+        }),
+        UsersModule,
+    ],
     controllers: [AppController],
     providers: [
         // 响应映射
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from "@nestjs/common";
+import { I18nService } from "nestjs-i18n";
 // import { CreateUserDto } from "./dto/create-user.dto";
 // import { UpdateUserDto } from "./dto/update-user.dto";
 
 @Injectable()
 export class UsersService {
+    // 引入I18n服务解析多语言
+    constructor(private readonly i18nService: I18nService) {}
     // create(createUserDto: CreateUserDto) {
     //     return "This action adds a new user";
     // }
 
-    findAll() {
-        return `This action returns all users`;
+    async findAll() {
+        // return `This action returns all users`;
+        return await this.i18nService.t("users.allUser");
     }
 
     findOne(id: number) {
